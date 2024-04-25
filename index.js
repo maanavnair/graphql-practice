3f4e39f0bc7fa033743852fcda1798dcd065fd39
Maanav Nair <maanavnair20042@gmail.com>
2024-04-25 20:07:06 +0530

    made typeDefs

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+//types
+import { typeDefs } from './schema.js';
+
 //server setup
 const server = new ApolloServer({
-
+    typeDefs,
+    //resolvers
 })
 
 const { url } = await startStandaloneServer(server, {
